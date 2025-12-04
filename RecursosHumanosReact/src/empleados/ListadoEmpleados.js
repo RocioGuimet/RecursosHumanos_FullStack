86b6ca0d1809e19a0e3b29daaee8d9f15efa9a03
unknown <rocioguimet@hotmail.com>
2025-12-04 17:51:23 -0300

    Configuro Axios para conectar con el backend

--- RecursosHumanosReact/src/empleados/ListadoEmpleados.js
+++ RecursosHumanosReact/src/empleados/ListadoEmpleados.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 export default function ListadoEmpleados() {
+
+    const urlBase = "http://localhost:8080/rh-app/empleados";
+
+    const[empleados, setEmpleados] = useState([]);
+
+    useEffect(() => {
+        cargarEmpleados();
+    }, []); //Para que se ejecute sólo una vez
+
+    const cargarEmpleados = async () => {
+        const resultado = await axios.get(urlBase);
+        console.log("Resultado cargar empleados");
+        console.log(resultado.data);
+        setEmpleados(resultado.data);
+    }
+
   return (
     <div className= "container">
         <div className="container text-center" style ={{margin: "30px"}}>
@@ -11,23 +28,22 @@ export default function ListadoEmpleados() {
             <tr>
             <th scope="col">Id</th>
             <th scope="col">Empleado</th>
-            <th scope="col">Departamento</th>
+            <th scope="col">Departamento</th>   
             <th scope="col">Sueldo</th>
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-            </tr>
-            <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-            </tr>
+            {
+            //Iteramos el Array Empleados
+            empleados.map((empleado, indice) => (
+                <tr key={indice}>
+                <th scope="row">{empleado.idEmpleado}</th>
+                <td>{empleado.nombre}</td>
+                <td>{empleado.departamento}</td>
+                <td>{empleado.sueldo}</td>
+                </tr>
+            ))
+            }
         </tbody>
         </table>
     </div>
